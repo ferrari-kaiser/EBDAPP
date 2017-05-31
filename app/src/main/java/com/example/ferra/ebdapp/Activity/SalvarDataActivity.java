68c39008d5016e8eb971742bbe4fa7760e8fb068
ferrari-kaiser <ferrari-kaiser@hotmail.com>
2017-05-31 01:46:48 -0300

    Criando SQLite

--- app/src/main/java/com/example/ferra/ebdapp/Activity/SalvarDataActivity.java
+++ app/src/main/java/com/example/ferra/ebdapp/Activity/SalvarDataActivity.java
@@ -5,9 +5,13 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.Spinner;
+import android.widget.Toast;
 
+import com.example.ferra.ebdapp.Controller.BancoController;
 import com.example.ferra.ebdapp.R;
 
 import java.util.Date;
@@ -25,6 +29,8 @@ public class SalvarDataActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+
+
         // capturando dado
         Intent intent = getIntent();
         Long dateSelected = intent.getLongExtra("dataLongMiliseconds", 0);
@@ -33,13 +39,36 @@ public class SalvarDataActivity extends AppCompatActivity {
         sp_hora_inicial = (Spinner) findViewById(R.id.sp_hora_inicial);
         sp_hora_final = (Spinner) findViewById(R.id.sp_hora_final);
 
+        Button botao = (Button)findViewById(R.id.btn_banner);
+
+        botao.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                BancoController crud = new BancoController(getBaseContext(), null);
+                sp_hora_inicial = (Spinner)findViewById(R.id.sp_hora_inicial);
+                sp_hora_final = (Spinner) findViewById (R.id.sp_hora_final);
+                EditText descricao = (EditText)findViewById((R.id.txt_salvar_data));
+                ImageView imagem = (ImageView)findViewById(R.id.img_banner);
+                String horaInicial = sp_hora_inicial.toString ();
+                String horaFinal = sp_hora_final.toString ();
+                String descricaoData = descricao.getText().toString();
+                String imagemData = imagem.toString ();
+                String resultado;
+
+                resultado = crud.insereDado(horaInicial,horaFinal,descricaoData,imagemData);
+
+                Toast.makeText(getApplicationContext(), resultado, Toast.LENGTH_LONG).show();
+            }
+        });
+    }
+
 
 //        // fazendo alguma coisa com o dado capturado
 //        TextView txt = (TextView) findViewById(R.id.txt_salvar_data);
 //        txt.setText(date.toString());
 
 
-    }
+
 
 //    public Spinner getsp_hora_inicial() {return sp_hora_inicial}
 
